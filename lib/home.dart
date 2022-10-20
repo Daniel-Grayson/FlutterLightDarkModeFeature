ae88578bbfc2a073e80049c6fcb3a5f7af5aed29
Daniel Grayson <graysoncodes@gmail.com>
2022-10-20 22:48:58 +0100

    Flutter light and dark mode feature

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Light & Dark Mode',
+          )),
+    );
+  }
+}
